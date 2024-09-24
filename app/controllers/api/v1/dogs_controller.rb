0c7bb9fae8cc5f0c7df4901321ea08346f97e1cc
Abduliokhanio <Akhan17@outlook.com>
2024-09-23 19:19:22 -0500

    initial ground work

---
+++ app/controllers/api/v1/dogs_controller.rb
@@ -0,0 +1,57 @@
+module Api
+  module V1
+    class DogsController < ApplicationController
+      before_action :set_dog, only: [:show, :update, :destroy]
+
+      # GET /dogs
+      def index
+        @dogs = Dog.all
+        render json: @dogs
+      end
+
+      # GET /dogs/:id
+      def show
+        if @dog
+          render json: @dog 
+        else 
+          render json: { error: 'Dog not found' }, status: :not_found #
+        end
+      end
+
+      # POST /dogs
+      def create
+        @dog = Dog.new(dog_params)
+        if @dog.save
+          render json: @dog, status: :created
+        else
+          render json: @dog.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PUT /dogs/:id
+      def update
+        if @dog.update(dog_params)
+          render json: @dog
+        else
+          render json: @dog.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /dogs/:id
+      def destroy
+        @dog.destroy
+        head :no_content
+      end
+
+      private
+
+      def set_dog
+        @dog = Dog.find(params[:id])
+      end
+
+      def dog_params
+        params.require(:dog).permit(:name, :img)
+      end
+    end
+  end
+end
---
+++ config/application.rb
@@ -0,0 +1,36 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module DogApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+    config.api_only = true
+
+    config.middleware.delete Webpacker::DevServerProxy
+    config.middleware.delete Rack::MiniProfiler
+    config.middleware.delete ActionDispatch::HostAuthorization
+    config.middleware.delete ActionDispatch::Static
+    config.middleware.delete Sprockets::Rails::QuietAssets
+    config.middleware.delete WebConsole::Middleware
+    config.middleware.delete ActionDispatch::ShowExceptions
+    config.middleware.delete ActionDispatch::DebugExceptions
+    config.middleware.delete Rack::Sendfile
+    config.middleware.delete ActionDispatch::ActionableExceptions
+
+
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
+end
