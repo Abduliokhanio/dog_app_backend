e14dc98981667630b4533ff20f3a7068ac520562
Abduliokhanio <akhan17@outlook.com>
2024-11-17 21:53:18 -0600

    new update to environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,9 +3,9 @@ const { environment } = require('@rails/webpacker')
 // Update the `node` section to align with the new Webpack configuration schema.
 environment.config.merge({
   node: {
-    global: true,
     __dirname: false,
     __filename: false,
+    global: true,
   },
 });
 
